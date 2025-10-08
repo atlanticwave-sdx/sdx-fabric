cfd57949c1eb8349225cb5dbfbd42bf660d8713f
Luis Marin <lmarinve@gmail.com>
2025-10-08 18:56:08 +0000

    fixing https in test

--- sdx-client/sdxclient/config.py
+++ sdx-client/sdxclient/config.py
@@ -3,7 +3,7 @@
 Configuration for SDXClient.
 
 This file may be auto-rewritten during GitHub Actions builds.
-- On TestPyPI builds → baked with SDX_BASE_URL = 'https://190.103.184.194'
+- On TestPyPI builds → baked with SDX_BASE_URL = 'http://190.103.184.194'
 - On PyPI (production) → remains as committed below.
 """
 
